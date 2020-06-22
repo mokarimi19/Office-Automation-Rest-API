7665cd46923b55748beb2ff3c2fa8b178b27b80d
mokarimi <mokarimi@outlook.com>
2020-06-22 06:56:56 +0430

    Django App jira: new urls added.

--- wise/urls.py
+++ wise/urls.py
@@ -9,6 +9,8 @@ router = routers.DefaultRouter()
 router.register(r"tasks", TaskViewSet)
 router.register(r"users", CustomUserViewSet)
 router.register(r"department", DepartmentViewSet)
+router.register(r"request", RequestViewSet)
+# router.register(r"dashboard", DashboardViewset)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -16,5 +18,11 @@ urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
+    path("users/<int:pk>/fire", FireUserAPI.as_view()),
+    path("dashboard/<int:pk>", DashboardUpdateViewset.as_view()),
+    path("dashboard/", DashboardListViewset.as_view()),
+
+    # path("users/<int:pk>/membership", MembershipRequestViewSet.as_view()),
+
     # url(r"users", UserCreate.as_view(), name="account-create"),
 ]
