d4675d390dc500c3feec2f34371aaf110edef212
mokarimi <mokarimi@outlook.com>
2020-06-22 06:25:07 +0430

    Django App jira: add first_name, last_name to user registeration

--- jira/managers.py
+++ jira/managers.py
@@ -2,22 +2,28 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
+    def create_user(self, username, email, first_name, last_name, password=None):
         if not username:
-            raise ValueError("Dude you need a username.")
+            raise ValueError("you need a username.")
         if not email:
             raise ValueError("type an e-mail..")
-
+        if not first_name:
+            raise ValueError("your first_name!?!")
+        if not last_name:
+            raise ValueError("fill out  last_name!?!")
         new_user = self.model(
-            username=username, email=CustomUserManager.normalize_email(email)
+            username=username, email=CustomUserManager.normalize_email(email),
+            first_name=first_name, last_name=last_name
         )
         new_user.set_password(password)
         new_user.is_staff = True
         new_user.save(using=self._db)
         return new_user
 
-    def create_superuser(self, username, email, password):
-        new = self.create_user(username, email, password=password)
+    def create_superuser(self, username, email, first_name, last_name, password):
+        new = self.create_user(
+            username, email, first_name, last_name, password=password
+        )
         new.is_active = True
         new.is_staff = True
         new.is_superuser = True
